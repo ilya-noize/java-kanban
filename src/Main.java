50da4483c483888d66e48bd3f1e91605a678f777
ilya-noize <shumakoff.ilya@yandex.ru>
2023-05-01 01:41:56 +0300

    -

--- src/Main.java
+++ src/Main.java
@@ -1,74 +1,78 @@
 import manager.Managers;
 import manager.TaskManager;
-import tasks.*;
+import tasks.Epic;
+import tasks.SubTask;
+import tasks.Task;
 
-import java.time.Duration;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
-import static tasks.Status.DONE;
-import static tasks.TypeTask.TASK;
+public class Main {
+    /*
+    private static final Integer ID_TASK_1 = 1;
+    private static final Integer ID_TASK_2 = 2;
+    */
+    private static final Integer ID_EPIC_1_FULL = 3;
+    /*
+    private static final Integer ID_SUBTASK_1_IN_EPIC1 = 4;
+    private static final Integer ID_SUBTASK_2_IN_EPIC1 = 5;
+    private static final Integer ID_SUBTASK_3_IN_EPIC1 = 6;
+    private static final Integer ID_EPIC_2_EMPTY = 7;
+     */
 
-public class Main{
-    protected LocalDateTime startTime;
-    protected Duration duration;
-    private static final String DATE_TIME_PATTERN = "dd.MM.yyyy HH:mm";
-    private static final DateTimeFormatter DATE_TIME = DateTimeFormatter.ofPattern(DATE_TIME_PATTERN);
+    public static TaskManager manager = Managers.getDefaultMemoryTask();
 
-
-    Main(){
-        //"31.12.2022 18:00", "PT360M"
-        this.startTime = LocalDateTime.parse("31.12.2022 18:00",DATE_TIME);
-        this.duration = Duration.parse("PT360M");
+    Main() {
     }
 
 
     public static void main(String[] args) {
-        addTasksTest();
-        SubTask subTask = manager.getSubTask(4); //st1
-        statusSubTaskIsChangeTo(DONE, subTask);
-        manager.getAllEpics().forEach(System.out::print);
+        addTasksSimple();
+        getPrioritizedTasks().forEach(System.out::print);
     }
 
-    public static TaskManager manager = Managers.getDefaultMemoryTask();
-    public static Map<TypeTask, List<Integer>> idTasks = new HashMap<>();
+    public static Set<Task> getPrioritizedTasks(){
+        Set<Task> prioritizedTasks = new TreeSet<>((task1, task2) -> {
+            LocalDateTime time1 = task1.getStartTime();
+            LocalDateTime time2 = task2.getStartTime();
 
-    private static void addTasksTest() {
-        idTasks.put(TASK, List.of(//"dd.MM.yyyy hh:mm"
-                manager.addTask(new Task(                   //1
-                        "Task 1", "Description by Task 1", "30.12.2022 19:00", "PT15M")),
-                manager.addTask(new Task(               //2
-                        "Task 2", "Description by Task 2", "30.12.2022 19:30", "PT30M"))
-        ));
-        int epicId = manager.addEpic(new Epic(              //3
-                "Epic 1", "Description by Epic 1", "31.12.2022 12:00", "PT100M"));
-        idTasks.put(TypeTask.SUBTASK, List.of(
-                manager.addSubTask(new SubTask(         //4
-                        "SubTask 1", "Description by SubTask 1", "31.12.2022 12:00", "PT20M", epicId)),
-                manager.addSubTask(new SubTask(         //5
-                        "SubTask 2", "Description by SubTask 2", "31.12.2022 12:30", "PT120M", epicId)),
-                manager.addSubTask(new SubTask(         //6
-                        "SubTask 3", "Description by SubTask 3", "31.12.2022 14:30", "PT20M", epicId))
-        ));
-        idTasks.put(TypeTask.EPIC, List.of(
-                epicId,
-                manager.addEpic(new Epic(                   //7
-                        "Epic 2", "Description by Epic 2", "31.12.2022 18:00", "PT360M"))
-        ));
+            if (time1 == null && time2 == null) {
+                return task1.getId() - task2.getId();
+            }
+
+            if (time1 == null) {
+                return 1;
+            }
+
+            if (time2 == null) {
+                return -1;
+            }
+
+            return time1.compareTo(time2);
+        });
+
+        prioritizedTasks.addAll(manager.getAllTasks());
+        prioritizedTasks.addAll(manager.getAllSubTasks());
+        prioritizedTasks.addAll(manager.getAllEpics());
+
+        return prioritizedTasks;
     }
-
-    public static void statusSubTaskIsChangeTo(Status status, SubTask subTask) {
-        manager.updateSubTask(new SubTask(
-                subTask.getId(),
-                subTask.getTitle(),
-                subTask.getDescription(),
-                status,
-                subTask.getStartTimeToString(),
-                subTask.getDurationToString(),
-                subTask.getEpicId())
-        );
+    private static void addTasksSimple() {
+        //1.2
+        manager.addTask(new Task("Task 1", "Description by Task 1",
+                "30.12.2022 19:00", "PT15M"));
+        manager.addTask(new Task("Task 2", "Description by Task 2",
+                "30.12.2022 19:30", "PT30M"));
+        //3
+        manager.addEpic(new Epic("Epic 1", "Description by Epic 1"));
+        //4.5.6
+        manager.addSubTask(new SubTask("SubTask 1", "Description by SubTask 1",
+                "31.12.2022 12:00", "PT20M", ID_EPIC_1_FULL));
+        manager.addSubTask(new SubTask("SubTask 2", "Description by SubTask 2",
+                "31.12.2022 12:30", "PT120M", ID_EPIC_1_FULL));
+        manager.addSubTask(new SubTask("SubTask 3", "Description by SubTask 3",
+                "31.12.2022 14:30", "PT20M", ID_EPIC_1_FULL));
+        //7
+        manager.addEpic(new Epic("Epic 2", "Description by Epic 2"));
     }
 }
