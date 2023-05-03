0e8c6e82fdc7f103467558a8702633b5208e65fd
ilya-noize <shumakoff.ilya@yandex.ru>
2023-05-03 15:28:09 +0300

    Refactoring code;

--- src/tests/manager/InMemoryTaskManagerTest.java
+++ src/tests/manager/InMemoryTaskManagerTest.java
@@ -4,8 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 
 class InMemoryTaskManagerTest extends TaskManagerTest<InMemoryTaskManager> {
-    @DisplayName(value = "Проведём подготовительные работы. " +
-            "Создадим менеджер для всех тестов.")
+    @DisplayName(value = "Подготовительные работы. " +
+            "Создание менеджера для всех тестов.")
     @BeforeEach
     public void beforeEach() {
         manager = new InMemoryTaskManager(Managers.getDefaultHistory());
