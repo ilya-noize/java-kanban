82c1eadc68c74ab271a62af7e8ca92acfbe47387
ilya-noize <shumakoff.ilya@yandex.ru>
2023-06-07 17:18:16 +0300

    Key-Value Client API

---
+++ src/http/client/KVTaskClient.java
@@ -0,0 +1,73 @@
+package http.client;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+public class KVTaskClient {
+    private final String serverURL;
+    private final String apiToken;
+
+    public KVTaskClient(String serverURL) throws IOException, InterruptedException {
+        this.serverURL = serverURL;
+
+        URI uri = URI.create(this.serverURL + "/register");
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(uri)
+                .header("Content-Type", "application/json")
+                .build();
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpResponse<String> response = client.send(request, //send throws IOException, InterruptedException
+                BodyHandlers.ofString());
+        this.apiToken = response.body();
+    }
+
+    public void put(String key, String json) {
+        URI uri = URI.create(this.serverURL + "/save/" + key + "?API_TOKEN=" + apiToken);
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .POST(HttpRequest.BodyPublishers.ofString(json))
+                .uri(uri)
+                .header("Content-Type", "application/json")
+                .build();
+
+        HttpClient client = HttpClient.newHttpClient();
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString(UTF_8));
+            if (response.statusCode() != 200) {
+                System.out.println("Не удалось сохранить данные");
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String load(String key) {
+        URI uri = URI.create(this.serverURL + "/load/" + key + "?API_TOKEN=" + apiToken);
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(uri)
+                .header("Content-Type", "application/json")
+                .build();
+
+        HttpClient client = HttpClient.newHttpClient();
+        try {
+            HttpResponse<String> response = client.send(request,
+                    HttpResponse.BodyHandlers.ofString(UTF_8)
+            );
+            return response.body();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            return "Во время запроса произошла ошибка";
+        }
+    }
+}
