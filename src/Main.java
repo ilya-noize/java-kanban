665ed376e549ed89b4a522a31ab88fa24fa380b3
ilya-noize <shumakoff.ilya@yandex.ru>
2023-06-07 17:32:48 +0300

    Test start

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,15 @@
+import http.client.KVTaskClient;
+import http.server.KVServer;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        new KVServer().start();
+        new KVTaskClient("http://localhost:8078/");
+
+        //HttpTaskServer httpTaskServer = new HttpTaskServer(
+        //        HttpServer.create(new InetSocketAddress(8080), 0));
+        //httpTaskServer.start();
     }
 }
