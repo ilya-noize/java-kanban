80f43c3a824e9b9b1b563782da7fc57c8f2c710c
ilya-noize <shumakoff.ilya@yandex.ru>
2023-06-14 11:28:08 +0300

    delete enum PATHS

--- src/http/Config.java
+++ src/http/Config.java
@@ -15,23 +15,4 @@ public class Config {
             return port;
         }
     }
-
-    public enum PATHS {
-        ROOT_TASK("/tasks"),
-        PATH_TASK(ROOT_TASK + "/task"),
-        PATH_SUBTASK(ROOT_TASK + "/subtask"),
-        PATH_SUBTASK_BY_EPIC(PATH_SUBTASK + "/epic"),
-        PATH_EPIC(ROOT_TASK + "/epic"),
-        PATH_HISTORY(ROOT_TASK + "/history");
-
-        final String path;
-
-        PATHS(String path) {
-            this.path = path;
-        }
-
-        public String get() {
-            return path;
-        }
-    }
 }
